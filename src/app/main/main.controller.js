3d43cfd2268f8da93f49a3d31b872e7a8e69e1eb
Boonjira Angsumalee <aii_aor@hotmail.com>
2015-10-13 16:49:17 +0700

    implement all basic features

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -11,18 +11,18 @@
 
     this.displayItems = function() {
       return _.filter(this.itemList, this.filterType);
-    }
+    };
 
     this.addItem = function(text) {
       if(text.trim()){
         this.itemList.push(new TodoItem(text));
         this.newItemText = '';
       }
-    }
+    };
 
     this.removeItem = function(index) {
       this.itemList.splice(index, 1);
-    }
+    };
 
     this.takeAll = function() {
       return true;
